4f692f29bbcf83ddd44226673dec6d8d720049c3
lakshitkalal <kalallakshit7@gmail.com>
2025-12-06 23:31:23 +0530

    Add EmailService class with email methods

---
+++ app/email_service.py
@@ -0,0 +1,8 @@
+class EmailService:
+    @staticmethod
+    async def send_verification_email(email: str, token: str):
+        pass
+    
+    @staticmethod
+    async def send_password_reset_email(email: str, token: str):
+        pass
